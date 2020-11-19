bc8d09decf336c8ea1b1887ef8ad579c861bf030
jubaan <julio.ab@outlook.com>
2020-11-19 12:59:57 -0600

    Add myHero and myNavbar

--- main.js
+++ main.js
@@ -4,10 +4,12 @@ import myHero from './my-hero';
 
 const content = document.querySelector(".content");
 const navbar = document.createElement("navbar");
-const hero = new myHero();
+const hero1 = document.createElement("hero");
+// const hero2 = new myHero();
+// const hero3 = new myHero();
 
 content.appendChild(navbar);
-content.appendChild(hero);
+content.appendChild(hero1);
 
 customElements.define('navbar', myNavbar);
 customElements.define('hero', myHero);
---
+++ my-navbar.js
@@ -0,0 +1,12 @@
+const template = document.createElement('template');
+
+template.innerHTML = `
+`;
+
+class myHero extends HTMLElements {
+  construct() {
+    super();
+  }
+}
+
+export default myNavbar;
