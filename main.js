ec45a8acc6b07bac462bf5798f960812a9e961ee
jubaan <julio.ab@outlook.com>
2020-11-19 10:11:55 -0600

    Add to main.js new modules

--- main.js
+++ main.js
@@ -1,1 +1,13 @@
 import switchTab from './switchTab';
+import myNavbar from './my-navbar';
+import myHero from './my-hero';
+
+const content = document.querySelector(".content");
+const navbar = document.createElement("navbar");
+const hero = new myHero();
+
+content.appendChild(navbar);
+content.appendChild(hero);
+
+customElements.define('navbar', myNavbar);
+customElements.define('hero', myHero);
