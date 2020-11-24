33e91699a93dc21275fb31a0f1d28eaa43ba9962
jubaan <julio.ab@outlook.com>
2020-11-23 18:58:55 -0600

    Add comments to index.js

--- src/index.js
+++ src/index.js
@@ -7,9 +7,11 @@ import HeroOurLocations from './hero-ourlocations';
 
 const content = document.querySelector('.content');
 
+// append elements to the index.html
 content.appendChild(new Navbar().navbar());
 content.appendChild(new HeroClub().render());
 content.appendChild(new HeroBreakfast().render());
 content.appendChild(new HeroOurLocations().render());
 
+// Module with the logic to swith tabs
 switchTab();
